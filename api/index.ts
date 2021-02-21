da983aa1a014edc46bb95761a82d2a99370f7b43
Jonathan Gustafsson <jonathan@gustafsson.work>
2021-02-21 21:52:02 +0100

    🚀

--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,36 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import CoinbasePro from "coinbase-pro";
-import invariant from "invariant";
-import { Decimal } from "decimal.js";
+import { NowRequest, NowResponse } from '@vercel/node';
+import CoinbasePro from 'coinbase-pro';
+import invariant from 'invariant';
 
-const key = process.env.API_KEY;
-const secret = process.env.SECRET;
+const key = process.env.API_KEY || '';
+const secret = process.env.SECRET || '';
 
-const apiURI = "https://api.pro.coinbase.com";
+const apiURI = 'https://api.pro.coinbase.com';
 
 type PaymentMethod = any;
-declare module "coinbase-pro" {
+declare module 'coinbase-pro' {
   export interface AuthenticatedClient {
     getPaymentMethods: () => PaymentMethod[];
   }
 }
 
-const cryptoMap = {
-  btc: "BTC-EUR",
-  eth: "ETH-EUR",
-  ltc: "LTC-EUR",
+const cryptoMap: Record<string, string> = {
+  btc: 'BTC-EUR',
+  eth: 'ETH-EUR',
+  ltc: 'LTC-EUR',
 };
 
-const purchase = async ({ crypto, passphrase, amount }) => {
+const purchase = async ({
+  crypto,
+  passphrase,
+  amount,
+}: {
+  crypto: string;
+  passphrase: string;
+  amount: number;
+}) => {
   if (amount > 300) {
-    throw new Error("Buy for less!");
+    throw new Error('Buy for less!');
   }
 
   const client = new CoinbasePro.AuthenticatedClient(
@@ -32,36 +39,40 @@ const purchase = async ({ crypto, passphrase, amount }) => {
     passphrase,
     apiURI
   );
-  const { price } = await client.getProductTicker(crypto);
-
-  const btcPrice = new Decimal(1).dividedBy(new Decimal(price));
-
-  const response = await client.placeOrder({
-    side: "buy",
-    price: new Decimal(amount).toFixed(2).toString(),
-    size: btcPrice.times(amount).toFixed(8).toString(),
+  const order = {
+    side: 'buy',
+    funds: amount,
     product_id: crypto,
-    type: "limit",
-  });
+    type: 'market',
+  };
+
+  console.log(order);
+  const response = await client.placeOrder(order);
+  console.log(response);
 };
 
 export default async (req: NowRequest, res: NowResponse) => {
+  console.log('body', req.body);
   try {
-    invariant(req.method === "POST", "Only POST supported");
-    invariant(typeof req.body?.passphrase === "string", "Missing passphrase");
-    invariant(typeof req.body?.amount === "number", "Missing amount");
-    invariant(cryptoMap[req.body?.crypto], "Did not find supported crypto");
+    invariant(req.method === 'POST', 'Only POST supported');
+    invariant(typeof req.body?.passphrase === 'string', 'Missing passphrase');
+    invariant(req.body?.amount, 'Missing amount');
+    invariant(cryptoMap[req.body?.crypto], 'Did not find supported crypto');
   } catch (e) {
+    console.error(e.message);
     return res.status(400).send(e.message);
   }
 
   try {
     await purchase({
       passphrase: req.body.passphrase,
-      amount: req.body.amount,
+      amount:
+        typeof req.body.amount === 'number'
+          ? req.body.amount
+          : parseInt(req.body.amount),
       crypto: cryptoMap[req.body.crypto],
     });
-    return res.status(200).send("Success");
+    return res.status(200).send('Success');
   } catch (e) {
     console.error(e);
     return res.status(400).send(e.data.message);
